d939022c34a35cdd30b75378ec40610deb787c6c
Devil-of-Chaos <ulbig.sebastian@googlemail.com>
2018-07-15 13:00:57 +0200

    Update box_footer.php

--- box_footer.php
+++ box_footer.php
@@ -5,7 +5,7 @@ print "
 	include('navi_footer.php');
 print "
 		<div class='copyright'>
-			&copy; 2018 | Powered by <a href='https://github.com/Devil-of-Chaos/ethminer-monitor' target='_blank' title='etherminer-monitor by Devil'>etherminer-monitor</a> | Donate to ETH: <a href='https://etherscan.io/address/0x34b8abe3399f25cf083489a5d1dbe9e545fc97d6' rel='nofollow' title='0x34b8abe3399f25cf083489a5d1dbe9e545fc97d6' target='_blank'>0x34b8abe3399f25cf083489a5d1dbe9e545fc97d6</a>
+			&copy; 2018 | Powered by <a href='https://github.com/Devil-of-Chaos/ethminer-monitor' target='_blank' title='etherminer-monitor by Devil'>ethminer-monitor</a> | Donate to ETH: <a href='https://etherscan.io/address/0x34b8abe3399f25cf083489a5d1dbe9e545fc97d6' rel='nofollow' title='0x34b8abe3399f25cf083489a5d1dbe9e545fc97d6' target='_blank'>0x34b8abe3399f25cf083489a5d1dbe9e545fc97d6</a>
 		</div><!-- /.copyright -->
 	</div><!-- /.shell -->
 </footer>";
